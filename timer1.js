2a2ed79301c6318dacc5daff2dd2215ae8095ec8
symphony <jciurc@gmail.com>
2022-04-12 18:48:47 -0600

    Updated function to take in parameter

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2).filter(Number);
 const intervals = args.map(a => Math.min(a + "000", 20000)); // clamp to 20 secs max, convert to ms)
 const sendBeep = () => process.stdout.write('\x07');
-const playBeeps = () => {
+const playBeeps = (intervals) => {
   for (const time of intervals)
     setTimeout(() => sendBeep(), time);
 };
 
-playBeeps();
+playBeeps(intervals);
