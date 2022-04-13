688b77116f56bbe4d90c9b977370b906244f40a9
symphony <jciurc@gmail.com>
2022-04-12 18:51:47 -0600

    Moved number sanitization to inside function

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
-const args = process.argv.slice(2).filter(Number);
-const intervals = args.map(a => Math.min(a + "000", 20000)); // clamp to 20 secs max, convert to ms)
+const args = process.argv.slice(2);
 const sendBeep = () => process.stdout.write('\x07');
-const playBeeps = (intervals) => {
+const playBeeps = (args) => {
+  const intervals = args.filter(Number).map(a => Math.min(a + "000", 20000)); // clamp to 20 secs max, convert to ms)
   for (const time of intervals)
     setTimeout(() => sendBeep(), time);
 };
 
-playBeeps(intervals);
+playBeeps(args);
