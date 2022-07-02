5648f9a44803b17d051c61aa54d21dcf4145e0ed
symphony <jciurc@gmail.com>
2022-07-01 21:25:01 -0600

    clean up timer 1 file. fix formatting. create timer2.js

--- timer1.js
+++ timer1.js
@@ -1,9 +1,12 @@
-const args = process.argv.slice(2);
-const sendBeep = () => process.stdout.write('\x07');
+const [, , ...args] = process.argv;
+const sendBeep = () => { process.stdout.write('\u0007'); };
+
 const playBeeps = (args) => {
-  const intervals = args.filter(Number).map(a => Math.min(a + "000", 20000)); // clamp to 20 secs max, convert to ms)
+  // sanitize inputs, clamp max interval time
+  const intervals = args.filter(Number).map((a) => Math.min(a + "000", 20000)); // clamp to 20 secs
+
   for (const time of intervals)
-    setTimeout(() => sendBeep(), time);
+    setTimeout(sendBeep, time);
 };
 
 playBeeps(args);
