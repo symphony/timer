8bd076fb606aede1687516b2bb233adc28d65950
symphony <jciurc@gmail.com>
2022-07-01 23:21:29 -0600

    add timeout in number inputs

--- timer2.js
+++ timer2.js
@@ -3,19 +3,18 @@ const rl = require('readline').createInterface({
   output: process.stdout,
   prompt: '> ',
 });
-
-rl.input.setRawMode(true);
 rl.input.setEncoding('utf8');
 
 const sendBeep = () => { process.stdout.write('\x07'); };
-
 const handleInput = (key) => {
+  // exit on ctrl + c
+  if (key === '\u0003') console.log() || process.exit();
+
   rl.output.write('\n' + rl.getPrompt());
-  if (key === '\u0003') process.exit();
   if (key === 'b') sendBeep();
+  if (key !== ' ' && !Number.isNaN(Number(key))) setTimeout(sendBeep, key + '000');
 };
 
-/** plays beeps */
 const playBeeps = () => {
   rl.prompt();
   rl.input.on('data', handleInput);
